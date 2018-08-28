e0956ff12323f572e4e3cc8cb234987b68c247e9
GisselleIb <gisselleib@ciencias.unam.mx>
2018-08-27 21:23:34 -0500

    servidor y cliente implementados de forma sencilla

--- src/cliente.py
+++ src/cliente.py
@@ -5,15 +5,46 @@ class Cliente():
     def __init__(self,host,port):
         self.host=host
         self.port=port
+        try:
+            self.sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        except socket.error as e:
+            print("Error al crear socket")
+            sys.exit()
+
+        def setHost(self,host):
+            self.host=host
+
+        def setPort(self,port):
+            self.port=port
+
+        def getHost(self):
+            return self.host
+
+        def getPort(self):
+            return self.port
 
     def seConecto(self):
-        pass
-
+        try:
+            self.sock.connect((self.host,self.port))
+            print("cliente conectado")
+            return True
+        except socket.timeout:
+            return False
     def desconectado(self):
         pass
 
     def enviado(self):
-        pass
-
+        print("Bienvenido, escribe tu mensaje!:"+"\n")
+        while True:
+            mensaje=input()
+            try:
+                self.sock.sendall(mensaje.encode())
+                print(mensaje)
+                return True
+            except socket.error:
+                print("Falla al enviar el mensaje")
+                sys.exit()
+                return False
     def respuesta(self):
-        pass
+        respuesta=self.sock.recv(4096)
+        print(respuesta)
--- src/servidor.py
+++ src/servidor.py
@@ -1,11 +1,12 @@
 import socket
 import sys
+from _thread import *
 class Servidor():
 
     def __init__(self,host,port):
         self.host=host
         self.port=port
-        self.sock=socket.socket()
+        self.sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
     def setHost(self,host):
         self.host=host
@@ -20,20 +21,29 @@ class Servidor():
         return self.port
 
     def conectaCliente(self):
-        return False
+        try:
+            self.sock.bind((self.host,self.port))
+            return True
+        except socket.error:
+            return False
+
+    def servidorVivo(self):
+        self.sock.listen(100)
+        while 1:
+            con,dir=self.sock.accept()
+            print("Conectado a"+dir[0]+":"+str(dir[1]))
+            start_new_thread(self.threadCliente,(con,))
+            
+
+    def threadCliente(self,con):
+        while True:
+            mensaje=con.recv(4096)
+            respuesta= ':'+ mensaje.decode()
+            if not mensaje:
+                break
+            con.sendall(mensaje)
+            print(respuesta)
+        con.close()
 
     def muerto(self):
-        return True
-
-    def escucha(self):
-        return ''
-
-
-    def acepta(self):
-        pass
-
-    def enviaRespuesta(self):
-        pass
-
-    def lee(self):
-        pass
+        self.sock.close()
