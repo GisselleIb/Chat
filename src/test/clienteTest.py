5e23936c6341803fcb90fa0b49c8facc163a5897
GisselleIb <gisselleib@ciencias.unam.mx>
2018-09-01 14:32:07 -0500

    cambio en las pruebas del cliente, se agregaron pruebas a setters y getters

--- src/test/clienteTest.py
+++ src/test/clienteTest.py
@@ -4,22 +4,36 @@ sys.path.append('../..')
 from src.cliente import Cliente
 class ClienteTest(unittest.TestCase):
 
-    def test_cliente(self):
-        pass
-
     def test_seConecto(self):
         client=Cliente(socket.gethostname(),65)
         self.assertFalse(client.seConecto())
-        client.setPort(80)
+        client.setPort(60000)
         self.assertTrue(client.seConecto())
     def test_desconectado(self):
         self.assertEqual("Desconectando del servidor",client.desconectado())
 
     def test_enviado(self):
-        client=Cliente(socket.gethostname(),80)
+        client=Cliente(socket.gethostname(),60000)
         assertFalse(client.seConecto())
 
-    def test_respuesta(self):
-        pass
+    def test_getHost(self):
+        serv=Servidor('',59243)
+        self.assertEqual(serv.getHost(),'')
+
+    def test_getPort(self):
+        serv=Servidor('',59243)
+        self.assertEqual(serv.getPort(),59243)
+
+    def test_setHost(self):
+        serv=Servidor('',59243)
+        self.assertEqual(serv.getHost(),'')
+        serv.setHost(socket.gethostname())
+        self.assertEqual(socket.gethostname(),serv.getHost())
+
+    def test_setPort(self):
+        serv=Servidor('',50000)
+        self.assertEqual(serv.getPort(),59243)
+        serv.setPort(8888)
+        self.assertEqual(serv.getPort(),8888)
 if __name__ == '__main__':
     unittest.main()
