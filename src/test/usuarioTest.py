082544c56f6f0a21b166104130e74db1caf266d1
GisselleIb <gisselleib@ciencias.unam.mx>
2018-09-10 20:23:40 -0500

    Cambios en las pruebas de agregaContacto de usuario y se agrego las pruebas de elimina contacto

--- src/test/usuarioTest.py
+++ src/test/usuarioTest.py
@@ -1,4 +1,5 @@
 import unittest
+import sys
 sys.path.append('../..')
 from src.usuario import Usuario
 class UsuarioTest(unittest.TestCase):
@@ -9,10 +10,17 @@ class UsuarioTest(unittest.TestCase):
     def test_setNombre(self):
         self.assertEqual(self.usuario.getNombre(),"name")
         self.usuario.setNombre("nombre")
-        self.assertEqual(self.usuario.getNombre(),"name")
+        self.assertEqual(self.usuario.getNombre(),"nombre")
         self.usuario.setNombre("name")
 
     def test_agregaContacto(self):
-
+        self.assertEqual(self.usuario.agregaContacto(None),
+        "Inserte un contacto válido")
+        self.usuario.agregaContacto("nombre")
+        self.assertTrue("nombre" in self.usuario.directorio)
 
     def test_eliminaContacto(self):
+        self.usuario.agregaContacto("N")
+        self.assertTrue("N" in self.usuario.directorio)
+        self.usuario.eliminaContacto("N")
+        self.assertFalse("N" in self.usuario.directorio)
